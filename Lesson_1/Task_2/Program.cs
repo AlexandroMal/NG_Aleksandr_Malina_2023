2c7caf69bb8122f6553833484b4618c66ae2f4df
Alexandr Malina <Sashamalina8695@gmail.com>
2023-03-17 19:55:43 +0200

    Add Task_2

---
+++ Lesson_1/Task_2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+Console.WriteLine("Enter number: ");
+
+try{
+
+    int inputNumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Result = {inputNumber + 10}");
+}
+
+catch
+{
+    Console.WriteLine("Incorrect value");
+}
+
+
