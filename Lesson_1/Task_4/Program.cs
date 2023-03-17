2f749c7aba0c79f7ec5cfc9dcbae6fcb1d3a81a9
Alexandr Malina <Sashamalina8695@gmail.com>
2023-03-17 19:56:01 +0200

    Add Task_4

---
+++ Lesson_1/Task_4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.Write("Enter first string: ");
+string str1 = Console.ReadLine();
+
+Console.Write("Enter second string: ");
+string str2 = Console.ReadLine();
+
+
+if (str1.Length > str2.Length)
+{
+    Console.WriteLine("The first string has more symbols than sting two.\n" +
+        $"Result: {str1 + str2}");
+}
+
+else if (str1.Length < str2.Length)
+{
+    string[] subs = str2.Split(str1[0]);
+    foreach (var sub in subs)
+    {
+        Console.WriteLine($"Substring {0+1}: {sub}");
+    }
+}
+
+else
+{
+    Console.WriteLine("The string are equal, so I return some void");
+}
