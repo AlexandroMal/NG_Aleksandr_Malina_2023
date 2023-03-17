40c89b4162bd32f3dd0e22401300e9770b555c42
Alexandr Malina <Sashamalina8695@gmail.com>
2023-03-17 19:55:55 +0200

    Add Task_3

---
+++ Lesson_1/Task_3/Program.cs
@@ -0,0 +1,57 @@
+﻿
+List<int> numbers = new List<int> ();
+
+while (numbers.Count < 10)
+{
+    Console.Write("Enter number: ");
+
+    try {
+        numbers.Add(Convert.ToInt32(Console.ReadLine()));
+    }
+
+    catch {
+        Console.WriteLine("Incorrect value, try again!");
+    }
+}
+
+
+while (true)
+{
+    Console.Write("Enter number for create dublicate:");
+
+    try
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        var indexValue = numbers.IndexOf(number);
+
+        if (indexValue != -1)
+        {
+            numbers.Insert(indexValue + 1, number);
+
+            // Output list
+            Console.Write("Your list: ");
+            foreach (int i in numbers)
+            {
+                Console.Write(i);
+            }
+            Console.WriteLine("");
+        }
+
+        else
+        {
+            Console.WriteLine("No such number is found in the list. Try again!");
+        }
+    }
+
+    catch
+    {
+        Console.WriteLine("Incorect valuy, try again!");
+    }
+
+}
+
+
+
+
+
