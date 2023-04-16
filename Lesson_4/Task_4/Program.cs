9d7c4c0a0fe0a40d7f1a595d01f45cf8a2ea9c36
Alexandr Malina <Sashamalina8695@gmail.com>
2023-04-16 18:19:01 +0300

    Add Task 4

---
+++ Lesson_4/Task_4/Program.cs
@@ -0,0 +1,12 @@
+﻿using Task_4.Interfaces;
+using Task_4.Vehicles;
+
+Vehicles car = new Vehicles();
+car.StartEngine();
+
+Vehicles motorcycle  = new Vehicles();
+motorcycle.StartEngine();
+
+Airplane airplane = new Airplane();
+airplane.StartEngine();
+airplane.Fly();
---
+++ Lesson_4/Task_4/Vehicles/Airplane.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Task_4.Interfaces;
+
+namespace Task_4.Vehicles
+{
+    public class Airplane : Vehicles, IFlyable
+    {
+        public void Fly()
+        {
+            Console.WriteLine("It's flying!");
+        }
+    }
+}
